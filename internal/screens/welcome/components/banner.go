8b7e325f184ce316a6139b7947910c7f84c3b0b4
mati <mateusz.meksula@gmail.com>
2026-03-11 18:19:09 +0100

    banner different styling

--- internal/screens/welcome/components/banner.go
+++ internal/screens/welcome/components/banner.go
@@ -1,18 +1,15 @@
 package components
 
 import (
-	"image/color"
 	"strings"
 
 	"charm.land/lipgloss/v2"
 )
 
 type BannerStyles struct {
-	Ascii                     lipgloss.Style
-	Descr                     lipgloss.Style
-	Version                   lipgloss.Style
-	VersionGradientColorStart color.Color
-	VersionGradientColorEnd   color.Color
+	Ascii   lipgloss.Style
+	Descr   lipgloss.Style
+	Version lipgloss.Style
 }
 
 type Banner struct {
@@ -30,11 +27,9 @@ func NewBanner(version string) Banner {
 
 	return Banner{
 		Styles: BannerStyles{
-			Ascii:                     lipgloss.NewStyle().MarginBottom(1),
-			Descr:                     lipgloss.NewStyle().Italic(true).Foreground(lipgloss.Color("#aaaaaa")),
-			Version:                   lipgloss.NewStyle().MarginBottom(2),
-			VersionGradientColorStart: lipgloss.Color("#12b3eb"),
-			VersionGradientColorEnd:   lipgloss.Color("#5460f9"),
+			Ascii:   lipgloss.NewStyle().Padding(1, 4).Background(lipgloss.Color("#303030")),
+			Descr:   lipgloss.NewStyle().Italic(true).Foreground(lipgloss.Color("#aaaaaa")),
+			Version: lipgloss.NewStyle().MarginBottom(2),
 		},
 		gopherTypeAscii: ascii,
 		descr:           "typing practise app for the terminal",
@@ -44,22 +39,10 @@ func NewBanner(version string) Banner {
 
 func (b Banner) View() string {
 	width := lipgloss.Width(b.gopherTypeAscii)
-	versionRaw := lipgloss.PlaceHorizontal(width, 0.15, b.version)
-	gradient := lipgloss.Blend1D(
-		lipgloss.Width(versionRaw),
-		b.Styles.VersionGradientColorStart,
-		b.Styles.VersionGradientColorEnd,
-	)
-
-	version := strings.Builder{}
-
-	for i, ch := range versionRaw {
-		version.WriteString(lipgloss.NewStyle().Background(gradient[i]).Render(string(ch)))
-	}
 
 	return lipgloss.JoinVertical(lipgloss.Center,
 		b.Styles.Ascii.Render(b.gopherTypeAscii),
-		b.Styles.Version.Render(version.String()),
+		lipgloss.PlaceHorizontal(width, 0.15, b.Styles.Version.Render(b.version)),
 		b.Styles.Descr.Render(b.descr),
 	)
 }
